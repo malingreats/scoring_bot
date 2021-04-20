3b9f99e2bdfeef3047bc4894f3d332d03ccee848
Inoxevious <mpasiinnocent@gmail.com>
2021-04-20 14:02:59 +0200

    first commit

--- app/main.py
+++ app/main.py
@@ -38,6 +38,7 @@ def compute_prediction(request_json):
 
 def predict(loan_data):
     model_two = '/app/XGBoost.sav'
+    
     model = pickle.load(open(model_two, 'rb'))
     client_infor = loan_data.values   #Subset a specific client infor, *a* represent SK_ID_CURR
     prob = model.predict_proba(client_infor).tolist()[0]    #predict a client's probability of defaulting
